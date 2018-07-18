5e917db74b98f8638ffe8bb51931c5e1d55faa0c
SARA EL AFIA <sara.elafia@artefact.com>
2018-07-18 11:43:28 +0200

    update webhooks to work with python 2.7 hmac lib

--- app/webhooks.py
+++ app/webhooks.py
@@ -1,4 +1,5 @@
 import hmac
+import hashlib
 from flask import request, Blueprint, jsonify, current_app
 from git import Repo
 
@@ -13,15 +14,16 @@ def handle_github_hook():
     signature = request.headers.get('X-Hub-Signature')
     sha, signature = signature.split('=')
 
-    secret = str.encode(current_app.config.get('GITHUB_SECRET'))
-
-    hashhex = hmac.new(secret, request.data, digestmod='sha1').hexdigest()
-    if hmac.compare_digest(hashhex, signature):
+    secret = str(current_app.config.get('GITHUB_SECRET'))
+    hmac_object = hmac.new(secret, request.data, digestmod=hashlib.sha1)
+    hashhex = hmac_object.hexdigest()
+    if hmac.compare_digest(hashhex, str(signature)):
         repo = Repo(current_app.config.get('REPO_PATH'))
         origin = repo.remotes.origin
         origin.pull('--rebase')
 
-        commit = request.json['after'][0:6]
-        print('Repository updated with commit {}'.format(commit))
+        if request.json.get('after'):
+        	commit = request.json['after'][0:6]
+        	print('Repository updated with commit {}'.format(commit))
 
     return jsonify({}), 200
